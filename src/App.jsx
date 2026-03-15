c9cc4781d824692d03ee6e411360a52c70eb17f9
Michael Oltarzewski <33165132+ozewski@users.noreply.github.com>
2026-03-15 17:48:08 -0400

    Add ButtonCounter

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { ButtonCounter } from "./ButtonCounter";
+
 const App = () => {
     return (
         <div>
             <h1>Hello world!</h1>
             <p>This is a test.</p>
-            <p>This is another test.</p>
+            <ButtonCounter />
         </div>
     )
 }
